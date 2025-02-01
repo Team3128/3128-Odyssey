dd5361b8dee3395fb9352a78d3421cdfd8a0898a
Audrey <audrey3zheng@gmail.com>
2025-02-01 15:43:20 -0800

    directory

--- NARASK/home_page/home_page.js
+++ NARASK/home_page/home_page.js
@@ -1,9 +1,8 @@
 data = [
-    {content: "Github Best Practices", keys: ["github", "code", "forks", "branches", "milestone", "request manager", "repository", "submodule", "directory", "push", "project", "pull"], value: 0},
+    {content: "Github Best Practices", keys: ["github", "code", "forks", "branches", "milestone", "request manager", "repository", "submodule", "directory", "push", "project", "pull"], value: 0, link: "/robot_github/robot_github.html"},
     {content: "Future Improvements", keys: ["minibot", "NARDash", "NARASK", "NARPit", "LED"], value: 0},
     {content: "Practicals", keys: ["orange pi", "micro sd", "balena etcher", "camera", "calibDB", "ChArUco", "Photon Vision"], value: 0},
     {content: "Robot-Github", keys: ["github", "code", "forks", "branches", "milestone", "request manager", "repository", "submodule", "directory", "push", "project", "pull"], value: 0},
-
 ];
 
 function test() {
@@ -25,6 +24,7 @@ function checkKeys(query) {
 }
 
 function sort() {
+    console.log("" + data.filter(obj => obj.value > 0).sort((a, b) => b.value - a.value))
     return data.filter(obj => obj.value > 0).sort((a, b) => b.value - a.value);
 }
 
@@ -38,8 +38,13 @@ function displayResults(results) {
     const resultsElement = document.getElementById('results');
     resultsElement.innerHTML = '';
     results.forEach(result => {
-        const listItem = document.createElement('li');
-        listItem.textContent = result.content;
-        resultsElement.appendChild(listItem);
+        var a = document.createElement('a');
+        var linkText = document.createTextNode(result.content);
+        a.appendChild(linkText);
+        a.title = result.content;
+        a.href = result.link;
+        resultsElement.appendChild(a);
+        var br = document.createElement('br');
+        resultsElement.appendChild(br);
     });
 }
