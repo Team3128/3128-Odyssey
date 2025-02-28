2b2a333bb95611e9255c8e0d01dd79a9634a5910
Audrey <audrey3zheng@gmail.com>
2025-02-28 01:43:32 -0800

    canpac data entries

--- js/NARASK/data.js
+++ js/NARASK/data.js
@@ -1,26 +1,10 @@
 data = [
     {
-        content: "Common",
-        category: "common",
-        keys: ["test", "test"],
-        summary: "test",
-        link: "/NARASK/common.html",
-        value: 0
-    },
-    {
-        content: "Practicals",
-        category: "practicals",
-        keys: ["test"],
-        summary: "test",
-        link: "/NARASK/practicals.html",
-        value: 0
-    },
-    {
         content: "Motor IDs",
         category: "practicals",
         keys: ["motor", "motors", "REV", "CTRE", "Pheonix Tuner", "Tuner", "Pheonix", "Rev Client", "ID", "DI", "motor ids", "swerve", "identification", "numbering", "CANCoder", "CAN", "PDH"],
         summary: "Guidelines for IDing using REV Client and Pheonix Tuner, including guidelines on what IDs to use for swerve, the CAN Chain, etc.",
-        link: "/NARASK/practicals.html#flashing-an-orange-pi",
+        link: "/NARASK/practicals.html#id'ing-swerve-devices",
         value: 0
     },
     {
@@ -32,56 +16,67 @@ data = [
         value: 0
     },
     {
-        content: "Github Best Practices", 
-        category: "github",
-        keys: ["github", "code", "forks", "branches", "milestone", "request manager", "repository", "submodule", "directory", "push", "project", "pull"], 
-        value: 0, 
-        link: "/NARASK/github.html",
-        summary: "tester"
+        content: "Cancoder Offsets",
+        category: "swerve practicals",
+        keys: ["CANCoder", "offset", "swerve", "modules"],
+        summary: "Steps to set CANCoder offsets in SwerveConstants and deploy.",
+        link: "/NARASK/practicals.html#cancoder-offsets",
+        value: 0
     },
-    // {
-    //     content: "Future Improvements", 
-    //     category: "other",
-    //     keys: ["minibot", "NARDash", "NARASK", "NARPit", "LED"], 
-    //     value: 0
-    // },
-    // {
-    //     content: "Practicals", 
-    //     category: "practicals",
-    //     keys: ["orange pi", "micro sd", "balena etcher", "camera", "calibDB", "ChArUco", "Photon Vision"],
-    //     value: 0
-    // },
-    // {
-    //     content: "Robot-Github", 
-    //     category: "common",
-    //     keys: ["github", "code", "forks", "branches", "milestone", "request manager", "repository", "submodule", "directory", "push", "project", "pull"], 
-    //     value: 0
-    // },
-
-    // {  
-    //     content: "test", 
-    //     category: "practicals",
-    //     keys: ["orange pi", "micro sd", "balena etcher", "camera", "calibDB", "ChArUco", "Photon Vision"], 
-    //     value: 0
-    // },
-
-    // {
-    //     content: "test1",  
-    //     category: "github",
-    //     keys: ["orange pi", "micro sd", "balena etcher", "camera", "calibDB", "ChArUco", "Photon Vision"], 
-    //     value: 0
-    // },
-    // {
-    //     content: "test2", 
-    //     category: "other",
-    //     keys: ["orange pi", "micro sd", "balena etcher", "camera", "calibDB", "ChArUco", "Photon Vision"], 
-    //     value: 0
-    // },
-     
-    // {
-    //     content: "test3", 
-    //     category: "common",
-    //     keys: ["orange pi", "micro sd", "balena etcher", "camera", "calibDB", "ChArUco", "Photon Vision"], 
-    //     value: 0
-    // },
+    {
+        content: "Odometry Doesn't Work",
+        category: "swerve practicals",
+        keys: ["odometry", "swerve", "CANCoder", "ID"],
+        summary: "Troubleshooting odometry issues related to motor and CANCoder IDs.",
+        link: "/NARASK/practicals.html#odometry-doesnt-work",
+        value: 0
+    },
+    {
+        content: "ID'ing Swerve Devices",
+        category: "swerve practicals",
+        keys: ["swerve", "ID", "devices", "modules"],
+        summary: "Guidelines on identifying swerve modules and assigning IDs.",
+        link: "/NARASK/practicals.html#iding-swerve-devices",
+        value: 0
+    },
+    {
+        content: "Finding Robot MOI",
+        category: "swerve practicals",
+        keys: ["MOI", "moment of inertia", "calculation"],
+        summary: "Steps to find the Moment of Inertia (MOI) using characterization commands.",
+        link: "/NARASK/practicals.html#finding-robot-moi",
+        value: 0
+    },
+    {
+        content: "Flashing an Orange Pi",
+        category: "vision practicals",
+        keys: ["Orange Pi", "flash", "SD card"],
+        summary: "Steps to flash an Orange Pi image onto an SD card.",
+        link: "/NARASK/practicals.html#flashing-an-orange-pi",
+        value: 0
+    },
+    {
+        content: "Flashing Cameras",
+        category: "vision practicals",
+        keys: ["camera", "flashing", "photonvision"],
+        summary: "Guide to flashing and calibrating cameras for PhotonVision.",
+        link: "/NARASK/practicals.html#flashing-cameras",
+        value: 0
+    },
+    {
+        content: "Radio Firmware Update",
+        category: "radio practicals",
+        keys: ["radio", "firmware", "update"],
+        summary: "Steps to update firmware on the VH-109 robot radio.",
+        link: "/NARASK/practicals.html#radio-firmware-update",
+        value: 0
+    },
+    {
+        content: "Robot Radio Configuration",
+        category: "radio practicals",
+        keys: ["robot radio", "configuration", "VH-109"],
+        summary: "Guide to configuring the VH-109 robot radio for team use.",
+        link: "/NARASK/practicals.html#robot-radio-configuration",
+        value: 0
+    }
 ];
