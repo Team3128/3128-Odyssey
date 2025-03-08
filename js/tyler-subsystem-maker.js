840c5ebf33c18de228f90661c72f65c93fb72109
Team 3128: Controls <35188310+ControlsNarwhal@users.noreply.github.com>
2025-03-07 18:14:56 -0800

    Update tyler-subsystem-maker.js

--- js/tyler-subsystem-maker.js
+++ js/tyler-subsystem-maker.js
@@ -1,18 +1,25 @@
 //hi 🤪
-function createSubsystem(typeBase, motors){
-    var subsystemCode = `
-    package common.core.subsystems;
+var voltageStart =  `
+package common.core.subsystems;
 
-    import java.util.List;
-    import java.util.function.Consumer;
+import java.util.List;
+import java.util.function.Consumer;
 
-    import common.hardware.motorcontroller.NAR_Motor;
-    import common.hardware.motorcontroller.NAR_Motor.Neutral;
-    import edu.wpi.first.wpilibj2.command.Command;
-    import edu.wpi.first.wpilibj2.command.SubsystemBase;
-    import static edu.wpi.first.util.ErrorMessages.requireNonNullParam;
-    `;
-    
+import common.hardware.motorcontroller.NAR_Motor;
+import common.hardware.motorcontroller.NAR_Motor.Neutral;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import static edu.wpi.first.util.ErrorMessages.requireNonNullParam;
+`;
+
+
+
+
+
+
+
+
+function createSubsystem(typeBase, motors){  
     return null;
 }
 
