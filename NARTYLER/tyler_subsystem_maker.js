37e9fc3dbaf3de4ab4e97e3b8b5040175a493c8a
Tyler Enderwick <tyler.enderwick@gmail.com>
2025-03-11 16:48:18 -0700

    worked on js and java

--- NARTYLER/tyler_subsystem_maker.js
+++ NARTYLER/tyler_subsystem_maker.js
@@ -1,14 +1,47 @@
 //hi 🤪
-var typeInput = document.getElementById("type");
-var motorInput = document.getElementById("type");
+//var typeInput = document.getElementById("type");
+var numberOfMotors = document.getElementById("num");
+var motors = [];
 var output = document.getElementById("output");
+var refreshButton = document.getElementById("sumbitNum");
 
 var variables =  `
-hi
+package frc.robot;
+
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 `;
 
 var help = `
-hi
+public class Subsystem extends SubsystemBase{
+
+    private static Subsystem subsystem;
+
+    public static Spark can;
+    public static VictorSP pwm;
+
+    public Subsystem(Spark can, VictorSP pwm) {
+        this.can = can;
+        this.pwm = pwm;
+    }
+
+    public Command run() {
+        return runOnce(
+        () -> {
+            //run something here
+        });
+    }
+
+    public Subsystem getInstance() {
+        if (subsystem == null) {
+            subsystem = new Subsystem(can, pwm);
+        }
+
+        return subsystem;
+    }
+}
 `;
 
 
@@ -23,7 +56,7 @@ function createSubsystem(typeBase, motors){
     var fullScript = `//copy and paste this:
     `;
     fullScript += variables;
-    fullScript += typeInput;
+    fullScript += help;
 
 
     output.innerText = fullScript;
@@ -37,6 +70,23 @@ createSubsystem(
     ["motor2","Kraken"]
     ]
 );
+
+function changeMotorInputs() {
+    if (motors.length > numberOfMotors) {
+        for (i = 0; i < motors.length - numberOfMotors; i++) {
+
+        }
+    } else if (motors.length < numberOfMotors) {
+        for (i = 0; i < numberOfMotors - motors.length; i++) {
+            
+        }
+    }
+
+    var motors = [];
+}
+
+setInterval(changeMotorInputs, 100);
+setInterval(createSubsystem, 100);
 
 //types we could have: velocity, voltage, position
 //motor types: kraken, neo, 
