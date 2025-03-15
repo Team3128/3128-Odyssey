f4bf63910716e459ac790032f243c8107b6f4d2c
Tyler Enderwick <tyler.enderwick@gmail.com>
2025-03-15 13:21:05 -0700

    making it assuming the user has common

--- NARTYLER/tyler_subsystem_maker.js
+++ NARTYLER/tyler_subsystem_maker.js
@@ -1,6 +1,7 @@
 //created by tyler enderwick
 var motorNumberInput = document.getElementById("num");
 var subsystemNameInput = document.getElementById("subName");
+var subsystemTypeInput = document.getElementById("subsystemType");
 var output = document.getElementById("output");
 var copyButton = document.getElementById("copy");
 
@@ -10,106 +11,20 @@ var numberOfMotors = 0;
 
 var copyScript = "";
 
-var variables =
-    `package frc.robot;
-
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkFlex;
-import edu.wpi.first.wpilibj.motorcontrol.PWMTalonFX;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-`;
+var variables = "";
 
 function runScriptMaker() {
-    var fullScript = ``;
+    var fullScript = `Hello, World!`;
     var subsystemName = subsystemNameInput.value;
-
-    fullScript += variables;
-
-    if (subsystemName == "") {
-        subsystemName = "CustomSubsystem";
-    }
-
-    fullScript += "public class " + subsystemName + " extends SubsystemBase {\n\n";
-    fullScript += "    private static " + subsystemName + " subsystem; \n\n"
-
-    for (var i = 0; i < numberOfMotors; i++) {
-        var motorName = "motor" + String(i + 1)
-        var motorType = document.getElementById(motorName).value;
-
-        if (motorType == "PWMSparkFlex") {
-            fullScript += "    public static PWMSparkFlex " + motorName + ";\n";
-        } else if (motorType == "PWMTalonFX") {
-            fullScript += "    public static PWMTalonFX " + motorName + ";\n";
-        } else {
-            fullScript += "    public static PWMSparkMax " + motorName + ";\n";
-        }
-    }
-
-    fullScript += "\n";
-    fullScript += `    public `+ subsystemName +`(`
-
-    for (var i = 0; i < numberOfMotors; i++) {
-        var motorName = "motor" + String(i + 1)
-        var motorType = document.getElementById(motorName).value;
-
-        if (motorType == "PWMSparkFlex") {
-            fullScript += "PWMSparkFlex " + motorName;
-        } else if (motorType == "PWMTalonFX") {
-            fullScript += "PWMTalonFX " + motorName;
-        } else {
-            fullScript += "PWMSparkMax " + motorName;
-        }
-
-        if (i != numberOfMotors - 1) {
-            fullScript += ", ";
-        }
-    }
-
-    fullScript += ") { \n";
-
-    for (var i = 0; i < numberOfMotors; i++) {
-        var motorName = "motor" + String(i + 1)
-
-        fullScript += "        this." + motorName + " = " + motorName + ";\n";
-    }
-
-    fullScript += `    }\n`;
-
-    fullScript += "\n";
-
-    fullScript +=
-        `    public Command exampleCommand() {
-         return runOnce(
-             //run something here
-             () -> {
-         });
-    }
-    \n`;
-
-    fullScript += 
-    `    public ` + subsystemName + ` getInstance() {
-            if (subsystem == null) {
-                subsystem = new Subsystem(`;
-
+    var subsystemType = subsystemTypeInput.value;
     
-    for (var i = 0; i < numberOfMotors; i++) {
-        var motorName = "motor" + String(i + 1)
+    if (subsystemType == "Voltage") {
 
-        fullScript += motorName
+    } else if (subsystemType == "Velocity") {
 
-        if (i != numberOfMotors - 1) {
-            fullScript += ", ";
-        }else{
-            fullScript += ");";
-        }
+    } else if (subsystemType == "Position") {
+
     }
-
-    fullScript += `}
-
-        return subsystem;
-    }
-}`;
 
     output.innerText = fullScript;
     copyScript = fullScript;
@@ -133,9 +48,8 @@ function motorInput() {
             motorDiv.innerHTML =
                 `<label for="motor` + motorNumber + `">Motor ` + motorNumber + `: </label>
                 <select id="motor` + motorNumber + `">
-                    <option value="PWMSparkFlex">PWMSparkFlex</option>
-                    <option value="PWMSparkMax">PWMSparkMax</option>
-                    <option value="PWMTalonFX">PWMTalonFX</option>
+                    <option value="Spark">Spark</option>
+                    <option value="TalonFX">TalonFX</option>
                 </select>
             `;
 
