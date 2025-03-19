a7014f2302ac6a2b7399b980e86c121cddb7b050
shravyamandadi <142697677+shravyamandadi@users.noreply.github.com>
2025-03-18 18:20:50 -0700

    FullBatteryList.js

--- js/NARPIT/batteries.js
+++ js/NARPIT/batteries.js
@@ -11,6 +11,98 @@
         {
             number: 3,
             type: "comp"
+        },
+        {
+            number: 4,
+            type: "comp"
+        },
+        {
+            number: 5,
+            type: "practice"
+        },
+        {
+            number: 6,
+            type: "comp"
+        },
+        {
+            number: 7,
+            type: "practice"
+        },
+        {
+            number: 8,
+            type: "practice"
+        },
+        {
+            number: 9,
+            type: "practice"
+        },
+        {
+            number: 10,
+            type: "comp"
+        },
+        {
+            number: 11,
+            type: "comp"
+        },
+        {
+            number: 12,
+            type: "practice"
+        },
+        {
+            number: 13,
+            type: "practice"
+        },
+        {
+            number: 14,
+            type: "comp"
+        },
+        {
+            number: 15,
+            type: "comp"
+        },
+        {
+            number: 16,
+            type: "comp"
+        },
+        {
+            number: 17,
+            type: "practice"
+        },
+        {
+            number: 19,
+            type: "comp"
+        },
+        {
+            number: 20,
+            type: "testbench"
+        },
+        {
+            number: 21,
+            type: "comp"
+        },
+        {
+            number: 22,
+            type: "comp"
+        },
+        {
+            number: 23,
+            type: "practice"
+        },
+        {
+            number: 25,
+            type: "practice"
+        },
+        {
+            number: 26,
+            type: "comp"
+        },
+        {
+            number: 27,
+            type: "comp"
+        },
+        {
+            number: 28,
+            type: "practice"
         }
     ]
 }
